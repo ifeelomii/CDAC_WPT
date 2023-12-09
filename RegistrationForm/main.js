8e4b04e5f78b80bc50c6a02092bbe8a41ab137df
Omkar Ware <123940891+ifeelomii@users.noreply.github.com>
2023-12-09 14:09:51 +0530

    Update main.js

--- RegistrationForm/main.js
+++ RegistrationForm/main.js
@@ -1,3 +1,4 @@
+var rno=0;
 function addTable() {
   var fname = document.getElementById("fname").value;
   var lname = document.getElementById("lname").value;
@@ -6,9 +7,23 @@ function addTable() {
   var state = document.getElementById("state").value;
   var education = document.querySelector(`input[type="checkbox"]:checked`).value;
   var genders = document.querySelector(`input[type="radio"]:checked`).value;
-  
+  var n =fname+" "+lname;
 
-  document.getElementById("tbody").innerHTML = "<th>" + fname+"</th>"+"<th>" + lname+"</th>"+"<th>" + address +"</th>"+"<th>" + state+"</th>"+"<th>" + education+"</th>"+"<th>" + genders+"</th>";
+  let row = document.getElementById("tbody").insertRow(rno);
+    let cell0 = row.insertCell(0);
+    let cell1 = row.insertCell(1);
+    let cell2 = row.insertCell(2);
+    let cell3 = row.insertCell(3);
+    let cell4 = row.insertCell(4);
+    
+    
+    cell0.innerHTML = n;
+    cell1.innerHTML = add;
+    cell2.innerHTML = city;
+    cell3.innerHTML = education;
+    cell4.innerHTML = gender;
+    rno++;
+  //document.getElementById("tbody").innerHTML = "<th>" + fname+"</th>"+"<th>" + lname+"</th>"+"<th>" + address +"</th>"+"<th>" + state+"</th>"+"<th>" + education+"</th>"+"<th>" + genders+"</th>";
 
   event.preventDefault();
     
